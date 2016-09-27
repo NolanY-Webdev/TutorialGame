e13666579135f2e9416b195cf77f7bfafcd9f5c0
Nolan Yee <skinnyfatman@gmail.com>
2016-09-27 11:17:40 -0700

    bug fixed, did have to do with clicking immediately as game began. did not have to do with resolution.

--- GameTutorial/src/data/Player.java
+++ GameTutorial/src/data/Player.java
@@ -27,6 +27,7 @@ public class Player {
 		this.index = 0;
 		this.waveManager = waveManager;
 		this.towerList = new ArrayList<TowerCannon>();
+		this.leftMouseButtonDown = true;
 	}
 
 	public void setTile() {
@@ -41,11 +42,10 @@ public class Player {
 		}
 		
 		//Mouse input
-//		if (Mouse.isButtonDown(0) && !leftMouseButtonDown) {
-//			
-//			towerList.add(new TowerCannon(QuickLoad("cannonBase"), grid.GetTile( (int) Math.floor(Mouse.getX() / 64), (int) Math.floor((HEIGHT - Mouse.getY() - 1) / 64)), 10, waveManager.getCurrentWave().getEnemyList(), 2, 700));
-////			setTile();
-//		}
+		if (Mouse.isButtonDown(0) && !leftMouseButtonDown) {
+			
+			towerList.add(new TowerCannon(QuickLoad("cannonBase"), grid.GetTile( (int) Math.floor(Mouse.getX() / 64), (int) Math.floor((HEIGHT - Mouse.getY() - 1) / 64)), 10, waveManager.getCurrentWave().getEnemyList(), 2, 700));
+		}
 		
 		leftMouseButtonDown = Mouse.isButtonDown(0);
 		
